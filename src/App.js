e831aa0e6fadaf6f7fe89c9b8ac4b91a159aadfb
OnatTuran <onatturann@gmail.com>
2024-11-03 18:32:38 -0800

    Full Stack Project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 style={{ fontWeight: 'bold' }}>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p style={{ fontWeight: 'bold' }}>101216227</p>
+        <p style={{ fontWeight: 'bold' }}>Onat Turan</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
